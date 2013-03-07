4f4f10726ed3ba02efa86159761c687c8256a13e
simons <simsampiah@yahoo.fr>
2013-03-07 10:51:39 +0000

    Finish layout and routes

---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# Returns the full title on a per-page basis			# Documentation comment
+  def full_title(page_title)							# Method invocation
+  	base_title = "Ruby on Rails Tutorial Sample App"	# Variable assignment
+  	if page_title.empty?								# Boolean test
+  	  base_title										# Implicit return
+  	else
+  	  "#{base_title} | #{page_title}"					# String interpolation
+  	end
+  end
